6b438bfcefa012cf452cf30275e9c56e07550927
yangjunwei <yuxiao9197@163.com>
2021-09-07 21:25:53 +0800

    1221. 分割平衡字符串

---
+++ src/com/yuxiao/BalancedStringSplit1221.java
@@ -0,0 +1,67 @@
+package com.yuxiao;
+
+/**
+ * 1221. 分割平衡字符串
+ * <p>
+ * 在一个 平衡字符串 中，'L' 和 'R' 字符的数量是相同的。
+ * <p>
+ * 给你一个平衡字符串s，请你将它分割成尽可能多的平衡字符串。
+ * <p>
+ * 注意：分割得到的每个字符串都必须是平衡字符串。
+ * <p>
+ * 返回可以通过分割得到的平衡字符串的 最大数量 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "RLRRLLRLRL"
+ * 输出：4
+ * 解释：s 可以分割为 "RL"、"RRLL"、"RL"、"RL" ，每个子字符串中都包含相同数量的 'L' 和 'R' 。
+ * 示例 2：
+ * <p>
+ * 输入：s = "RLLLLRRRLR"
+ * 输出：3
+ * 解释：s 可以分割为 "RL"、"LLLRRR"、"LR" ，每个子字符串中都包含相同数量的 'L' 和 'R' 。
+ * 示例 3：
+ * <p>
+ * 输入：s = "LLLLRRRR"
+ * 输出：1
+ * 解释：s 只能保持原样 "LLLLRRRR".
+ * 示例 4：
+ * <p>
+ * 输入：s = "RLRRRLLRLL"
+ * 输出：2
+ * 解释：s 可以分割为 "RL"、"RRRLLRLL" ，每个子字符串中都包含相同数量的 'L' 和 'R' 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/split-a-string-in-balanced-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author yuxiao
+ * @date 2021-09-07
+ */
+public class BalancedStringSplit1221 {
+
+    public static void main(String[] args) {
+        System.out.println(balancedStringSplit("RLLLLRRRLR"));
+        System.out.println(balancedStringSplit("RLRRLLRLRL"));
+        System.out.println(balancedStringSplit("LLLLRRRR"));
+        System.out.println(balancedStringSplit("RLRRRLLRLL"));
+    }
+
+
+    public static int balancedStringSplit(String s) {
+        int result = 0;
+        int len = 0;
+        // 当R和L的对len的操作为0时，说明当前一个子串是平衡的
+        for (int i = 0; i < s.length(); i++) {
+            len += s.charAt(i) == 'R' ? 1 : -1;
+            if (len == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+}
